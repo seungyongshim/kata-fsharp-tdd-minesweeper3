16cbe4db30569767c94a857804b8f07f0b82ec01
SeungYongShim <syshim@me.com>
2022-05-18 22:40:55 +0900

    .

--- src/Minesweeper.Tests/CellSpec.fs
+++ src/Minesweeper.Tests/CellSpec.fs
@@ -8,5 +8,9 @@ open Minesweeper.Cell
 [<Fact>]
 let Should_be_Covered () =
     let sut = init
-    
     Assert.Equal(".", sut |> string)
+
+[<Fact>]
+let Should_be_One () =
+    let sut = One
+    Assert.Equal("1", sut |> string)
