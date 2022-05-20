92aef1434b6c461d3d0e8bc2f58dac8d39232ac1
SeungYongShim <syshim@me.com>
2022-05-21 06:40:26 +0900

    .

--- src/Minesweeper/Minefield.fs
+++ src/Minesweeper/Minefield.fs
@@ -2,6 +2,7 @@ namespace Minesweeper
 
 open System.Text
 open FSharpPlus
+open Cell
 
 type Minefield =
     | Setup of width: int * height: int
@@ -14,7 +15,7 @@ module Minefield =
             let cells = Map.ofSeq <| seq {
                 for y in {0..h - 1} do
                 for x in {0..w - 1} do
-                yield ((y, x), Cell.init)
+                yield ((y, x), init)
             }
             Playing (w, h, cells)
         | _ -> v
@@ -22,7 +23,7 @@ module Minefield =
         match v with
         | Playing (w, h, z) -> 
             (StringBuilder(), z |> Map.toSeq) ||> fold (fun s ((y, x), c) ->
-                let _1 = s.Append (c |> Cell.char)
+                let _1 = s.Append (c |> char)
                 match x with
                 | _ when x = w - 1 -> _1.Append '\n'
                 | _ -> _1.Append ' ') |> string
