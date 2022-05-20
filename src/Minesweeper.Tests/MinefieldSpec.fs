9fef14d77d52d02fcd892b4fb31a76902b5005f6
SeungYongShim <syshim@me.com>
2022-05-21 07:55:58 +0900

    .

--- src/Minesweeper.Tests/MinefieldSpec.fs
+++ src/Minesweeper.Tests/MinefieldSpec.fs
@@ -12,13 +12,13 @@ let Should_be_Create () =
 
 [<Fact>]
 let Should_be_Create_With_Bombs () =
-    let sut = SetupWithBombs (4, 3, seq {(1, 0)}) |> start
-    Assert.Equal(1, sut |> cells
+    let sut = SetupWithBombs (4, 3, seq {(1, 0); (0, 2)}) |> start
+    Assert.Equal(2, sut |> cells
                         |> Map.toSeq
                         |> Seq.filter (fun ((y, x), c) -> c |> Cell.isBomb)
                         |> Seq.length)
 
 [<Fact>]
 let Should_be_Create_With_Bombs2 () =
-    let sut = SetupWithBombs (4, 3, seq {(1, 0)}) |> start
-    Assert.Equal("1 1 0 0\n* 1 0 0\n1 1 0 0\n", (sut, Cell.charUncovered) ||> string)
+    let sut = SetupWithBombs (4, 3, seq {(1, 0); (0, 2)}) |> start
+    Assert.Equal("1 2 * 1\n* 2 1 1\n1 1 0 0\n", (sut, Cell.charUncovered) ||> string)
